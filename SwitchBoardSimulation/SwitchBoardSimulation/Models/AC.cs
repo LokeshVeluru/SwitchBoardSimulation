fb7428f8d9008c0a3598991b90f733c478aec0b4
V LOKESH <49988191+LokeshVeluru@users.noreply.github.com>
2021-03-31 09:28:19 +0530

    Add files via upload

---
+++ SwitchBoardSimulation/SwitchBoardSimulation/Models/AC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwitchBoardSimulation.Models
+{
+    class AC : Appliance
+    {
+        public AC(int id)
+        {
+            this.Type = ApplianceType.AC;
+            this.ApplianceId = "A-" + id.ToString();
+        }
+    }
+}
---
+++ SwitchBoardSimulation/SwitchBoardSimulation/Models/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwitchBoardSimulation.Models
+{
+   public enum ApplianceType
+    {
+        FAN = 1,
+        AC = 2,
+        BULB = 3
+    }
+    public enum SwitchStatus
+    {
+        OFF,
+        ON
+    }
+}
